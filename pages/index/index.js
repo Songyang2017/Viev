0e880f984129673249de2346f273a326cf28f2f3
Songyang2017 <1546302993@qq.com>
2018-08-16 00:09:37 +0800

    12

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,15 @@
-// 引用百度地图微信小程序JSAPI模块 
-var bmap = require('../../libs/bmap-wx.js');
 Page({
-  data: {
-    weatherData: ''
+  data:{
+    btns:[]
   },
-  onLoad: function () {
-    var that = this;
-    // 新建百度地图对象 
-    var BMap = new bmap.BMapWX({
-      ak: 'AeeTrXplpLx07vZ7ZP0VnPBHnVtpGMhC'
-    });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      var weatherData = data.currentWeather[0];
-      var detailData = data.originalData.results[0].weather_data;
-      console.log(data)
-      // weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
-      that.setData({
-        weatherData: weatherData,
-        detailData: detailData
-      });
-    }
-    // 发起weather请求 
-    BMap.weather({
-      fail: fail,
-      success: success
-    });
+  weather:function(){
+    wx.navigateTo({
+      url:'../weather/weather'
+    })
+  },
+  apiSetting:function(){
+    wx.navigateTo({
+      url: '../api/api',
+    })
   }
 })
